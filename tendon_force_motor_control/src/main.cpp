a77439ce224631d5cb7ffed3b1b473e50970ff6a
eatiller <70362684+eatiller@users.noreply.github.com>
2026-07-29 20:01:36 -0500

    Cleaned up Setup and global variables in main

--- tendon_force_motor_control/src/main.cpp
+++ tendon_force_motor_control/src/main.cpp
@@ -1,32 +1,130 @@
+/* -------------------------------------
+ * Includes
+ * ------------------------------------- */
+
 #include <Arduino.h>
+#include <SPI.h>
+
+// ODrive
 #include "ODriveCAN.h"
+#include <FlexCAN_T4.h>
+#include "ODriveFlexCAN.hpp"
+
+// Encoder
 #include "encoder.hpp"
 #include "spi_encoder.hpp"
-#include <SPI.h>
+
+// Load Cell
 #include "ADS1220.h"
 
-// Pin Defines
+/* -------------------------------------
+ * Motors On/Off & Nodes
+ * ------------------------------------- */
+
+#define MOTORS_ON
+#define ODRV0_NODE_ID 0
+
+/* -------------------------------------
+ * Communication Baudrates
+ * ------------------------------------- */
+
+// CAN bus baudrate. Make sure this matches for every device on the bus
+#define CAN_BAUDRATE 250000
+// #define SPI_BAUDRATE 1000000   -- Manually set inside libraries
+#define SERIAL_BAUDRATE 115200
+
+
+/* -------------------------------------
+ * Pin Defines
+ * ------------------------------------- */
+
+// Loadcell SPI Pin Defines
 #define LOADCELL_CS_PIN 21
-#define ENCODER_CS_PIN 10
 #define DRDY_PIN 9
 
-// Global Vars 
-// (parameters and testbed parameters)
+// Encoder SPI Pin Defines
+#define ENCODER_CS_PIN 10
+
+/* -------------------------------------
+ * Loadcell Defines
+ * ------------------------------------- */
+
+// Num sample for determining the zero offset of the load cell
+#define OFFSET_SAMPLES 2000
+
+// Load Cell calibration factor: (Vref / gain) / (2^23) — tune to your load cell (N/tick)
+const float LOAD_CELL_SCALE  = 0.07007488819976268f;
+const float LOAD_CELL_OFFSET = 49.394776217565585f;
+
+
+/* -------------------------------------
+ * ODRIVE Setup
+ * ------------------------------------- */
+
+#ifdef MOTORS_ON
+void onCanMessage(const CanMsg& msg);
+bool setupCan();
+void onHeartbeat(Heartbeat_msg_t& msg, void* user_data);
+void onFeedback(Get_Encoder_Estimates_msg_t& msg, void* user_data);
+void onCanMessage(const CanMsg& msg);
+#endif
+
+/* -------------------------------------
+ * ODRIVE Structures
+ * ------------------------------------- */
+#ifdef MOTORS_ON
+struct ODriveUserData {
+  Heartbeat_msg_t last_heartbeat;
+  bool received_heartbeat = false;
+  Get_Encoder_Estimates_msg_t last_feedback;
+  bool received_feedback = false;
+};
+
+struct ODriveStatus; // hack to prevent teensy compile error
+#endif
+
+/* -------------------------------------
+ * Object Instantiation
+ * ------------------------------------- */
+#ifdef MOTORS_ON
+FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> can_intf;   // Using CAN3 on Teensy
+ODriveCAN odrv0(wrap_can_intf(can_intf), ODRV0_NODE_ID); // Standard CAN message ID
+ODriveCAN* odrives[] = {&odrv0}; // Make sure all ODriveCAN instances are accounted for here
+#endif
+
+// Load Cell Related
+ADS1220 adc(LOADCELL_CS_PIN, DRDY_PIN);
+
+/* -------------------------------------
+ * Object Instantiation
+ * ------------------------------------- */
+SPIEncoder* AS5047P = nullptr;
+Angle encoder_angle;  // rotations=0, radians=0, direction=1 by default
+
+/* -------------------------------------
+ * Global Vars
+ * ------------------------------------- */
+#ifdef MOTORS_ON
+ODriveUserData odrv0_user_data;   // Keep some application-specific user data for every ODrive.
+#endif
+
+// Load Cell Related
+volatile bool adcDataReady = false;
+
+// Encoder Related
+float center = 0.0f;
+
+// Controller Parameters
 const float m_v = 1378.125f;
 const float b = 36750.0f;
-const float k = 500e3f;
 const float PULLEY_RADIUS = 0.0089f;    // in meters
-const float GEAR_RATIO = 146.0f;    // for assistive torque control
+const float GEAR_RATIO = 146.0f;
 
 // Control Vars -- Pre-defined to minimize loop time
 float x_ref = 0.0f;
 float xdot_ref = 0.0f;
 float F_ref = 0.0f;
 float xddot = 0.0f;
-
-float F_ref_array[] = {};
-int N_SAMPLES = 0;
-int sample_index = 0;
 
 // Time Control vars
 uint32_t time_zero = 0;
@@ -45,71 +143,9 @@ float torque_feedforward = 0;
 float loadcell_read_time_us = -1.0f;
 float weight = 0.0f;
 
-
-
-//Start ADC
-ADS1220 adc(LOADCELL_CS_PIN, DRDY_PIN);
-
-// Load Cell data flag setting
-volatile bool adcDataReady = false;
-
 void onDRDY() {
   adcDataReady = true;
 }
-
-// Load Cell calibration factor: (Vref / gain) / (2^23) — tune to your load cell
-const float LOAD_CELL_SCALE  = 0.07007488819976268f;
-const float LOAD_CELL_OFFSET = 49.394776217565585f;
-
-// Control Array (Target Forces)
-
-// Modified Example Code derived from ODrive's Website
-
-// Documentation for the example can be found here:
-// https://docs.odriverobotics.com/v/latest/guides/arduino-can-guide.html
-
-// CAN bus baudrate. Make sure this matches for every device on the bus
-#define CAN_BAUDRATE 250000
-
-// ODrive node_id for odrv0
-#define ODRV0_NODE_ID 0
-
-// Uncomment below the line that corresponds to your hardware.
-// See also "Board-specific settings" to adapt the details for your hardware setup.
-
-#define IS_TEENSY_BUILTIN // Teensy boards with built-in CAN interface (e.g. Teensy 4.1). See below to select which interface to use.
-
-/* Board-specific includes ---------------------------------------------------*/
-
-#if defined(IS_TEENSY_BUILTIN) + defined(IS_ARDUINO_BUILTIN) + defined(IS_MCP2515) + defined(IS_STM32_BUILTIN) + defined(IS_ESP32_TWAI) != 1
-#warning "Select exactly one hardware option at the top of this file."
-
-#if CAN_HOWMANY > 0 || CANFD_HOWMANY > 0
-#define IS_ARDUINO_BUILTIN
-#warning "guessing that this uses HardwareCAN"
-#else
-#error "cannot guess hardware version"
-#endif
-
-#endif
-
-#ifdef IS_TEENSY_BUILTIN
-// See https://github.com/tonton81/FlexCAN_T4
-// clone https://github.com/tonton81/FlexCAN_T4.git into /src
-#include <FlexCAN_T4.h>
-#include "ODriveFlexCAN.hpp"
-struct ODriveStatus; // hack to prevent teensy compile error
-#endif // IS_TEENSY_BUILTIN
-
-/* Board-specific settings ---------------------------------------------------*/
-
-void onCanMessage(const CanMsg& msg);
-
-/* Teensy */
-
-#ifdef IS_TEENSY_BUILTIN
-
-FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> can_intf;   // Using CAN3 on Teensy
 
 bool setupCan() {
   can_intf.begin();
@@ -120,27 +156,6 @@ bool setupCan() {
   can_intf.onReceive(onCanMessage);
   return true;
 }
-
-#endif // IS_TEENSY_BUILTIN
-
-// Instantiate ODrive objects
-ODriveCAN odrv0(wrap_can_intf(can_intf), ODRV0_NODE_ID); // Standard CAN message ID
-ODriveCAN* odrives[] = {&odrv0}; // Make sure all ODriveCAN instances are accounted for here
-
-struct ODriveUserData {
-  Heartbeat_msg_t last_heartbeat;
-  bool received_heartbeat = false;
-  Get_Encoder_Estimates_msg_t last_feedback;
-  bool received_feedback = false;
-};
-
-// Keep some application-specific user data for every ODrive.
-ODriveUserData odrv0_user_data;
-
-// Encoder Defines
-float center = 0.0f;
-SPIEncoder* AS5047P = nullptr;
-Angle encoder_angle;  // rotations=0, radians=0, direction=1 by default
 
 // Called every time a Heartbeat message arrives from the ODrive
 void onHeartbeat(Heartbeat_msg_t& msg, void* user_data) {
@@ -167,10 +182,22 @@ void onCanMessage(const CanMsg& msg) {
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  delay(200);
 
+/* -------------------------------------
+ * Setup
+ * ------------------------------------- */
+void setup() {  
+  Serial.begin(115200);
+  // Wait for serial port to open
+  while(!Serial){
+    delay(10);
+  }
+
+  /* ---------
+   * Configure ODrive
+   */
+
+  #ifdef MOTORS_ON
   Serial.println("Starting ODriveCAN...");
 
   // Register callbacks for the heartbeat and encoder feedback messages
@@ -187,9 +214,10 @@ void setup() {
   Serial.println("Waiting for ODrive...");
   while (!odrv0_user_data.received_heartbeat) {
     pumpEvents(can_intf);
+    delay(1);
   }
 
-  Serial.println("found ODrive");
+  Serial.println("Found ODrive");
 
   // request bus voltage and current (1sec timeout)
   Serial.println("attempting to read bus voltage and current");
@@ -204,49 +232,13 @@ void setup() {
   Serial.print("DC current [A]: ");
   Serial.println(vbus.Bus_Current);
 
-  Serial.println("Setting up AS5047P Encoder...");
-  pinMode(ENCODER_CS_PIN, OUTPUT);
-  digitalWrite(ENCODER_CS_PIN, HIGH);
-  AS5047P = new SPIEncoder(EncoderReadCmd, SPI, ENCODER_CS_PIN);
-
-  Serial.println("Initializing ADS1220...");
-
-  adc.begin();
-  pinMode(DRDY_PIN, INPUT_PULLDOWN);
-  attachInterrupt(digitalPinToInterrupt(DRDY_PIN), onDRDY, FALLING);  // ADS1220 pulls DRDY low when data's ready
-  adc.reset();
-  delay(100);
-
-  Serial.println("ADS1220 reset done");
-
-  adc.writeRegister(0x00, 0x2E);
-  adc.writeRegister(0x01, 0xD4);
-  delay(10);
-
-  // Initialize Data
-  N_SAMPLES = sizeof(F_ref_array) / sizeof(F_ref_array[0]);
-
-    /*
-    Serial.println("Registers written");
-    Serial.print("Reg0 readback (should be 2E): 0x");
-    Serial.println(adc.readRegister(0x00), HEX);
-    Serial.print("Reg1 readback (should be D4): 0x");
-    Serial.println(adc.readRegister(0x01), HEX);
-    */
-
-
-    // Wait for up to 6 seconds for the serial port to be opened on the PC side.
-    // If no PC connects, continue anyway.
-    // for (int i = 0; i < 60; ++i) {
-    //    delay(100);
-    //}
-
-  //ODrive
   Serial.println("Enabling closed loop control...");
 
   while (odrv0_user_data.last_heartbeat.Axis_State != ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL) {
     odrv0.clearErrors();
     delay(1);
+    odrv0.setAbsolutePosition(0.0f);
+    odrv0.setControllerMode(CONTROL_MODE_POSITION_CONTROL, INPUT_MODE_PASSTHROUGH);
     odrv0.setState(ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL);
 
     // Pump events for 150ms. This delay is needed for two reasons;
@@ -264,15 +256,6 @@ void setup() {
 
   Serial.println("ODrive running!");
 
-  adc.startConversion();
-  delay(100);
-  // Serial.println("Conversion started, waiting for DRDY...");
-
-  // Zero the offset with 100 samples --> sets the initial position as zero.
-  adc.findADCOffset(2000);
-
-  Serial.println("ADS1220 Setup Complete");
-
   // wait for the first ODRIVE encoder feedback
   while (!odrv0_user_data.received_feedback) {
     pumpEvents(can_intf);
@@ -286,6 +269,30 @@ void setup() {
   Serial.println(center);
 
   Serial.println("Starting Control Loop...");
+
+  #endif
+
+  Serial.println("Setting up AS5047P Encoder...");
+  pinMode(ENCODER_CS_PIN, OUTPUT);
+  digitalWrite(ENCODER_CS_PIN, HIGH);
+  AS5047P = new SPIEncoder(EncoderReadCmd, SPI, ENCODER_CS_PIN);
+
+  Serial.println("Initializing ADS1220...");
+
+  adc.begin();
+  pinMode(DRDY_PIN, INPUT_PULLDOWN);
+  attachInterrupt(digitalPinToInterrupt(DRDY_PIN), onDRDY, FALLING);  // ADS1220 pulls DRDY low when data's ready
+  adc.reset();
+
+  delay(100);
+
+  adc.writeRegister(0x00, 0x2E);
+  adc.writeRegister(0x01, 0xD4);    // 2kHz
+  adc.startConversion();
+  
+  adc.findADCOffset(OFFSET_SAMPLES);
+
+  Serial.println("Entering main loop");
 }
 
 void loop() {
@@ -297,6 +304,18 @@ void loop() {
                         // Enabled because of Teensy + FlexCAN Implementation
 
   static uint32_t last_print = 0;
+
+  while (digitalRead(DRDY_PIN)) {}    //wait for drdy to trip before proeceding
+
+  // Read loadcell ever loop. we only continue our loop when we have data.
+  adcDataReady = false;
+  weight = adc.readDataCalibrated(LOAD_CELL_SCALE) + LOAD_CELL_OFFSET;
+  loadcell_read_time_us = millis();
+  //Serial.print("time: ");
+  //Serial.print(millis() / 1000.0, 3);
+  //Serial.print(" weight: ");
+  //Serial.println(weight, 4);
+  
 
   // Time control
 
@@ -319,7 +338,7 @@ void loop() {
   // Feed ODrive: x_ref and xdot (and also a caluculated torque feed forward)
   // x_ref isn't something we care about much -- it's not going to matter much
   
-  // Live Calculate Force
+  // Live Calculate Desired Force
   F_ref = (100.0f * sin((PI/10) * ((millis()/1000.0f) - (time_zero/1000.0f)))) + 100.0f;
 
   xddot = (F_ref - (b * xdot_ref) - (k * x_ref))/m_v;
@@ -333,7 +352,7 @@ void loop() {
   torque_feedforward = (F_ref * PULLEY_RADIUS) / GEAR_RATIO;
 
   //Command ODrive to move motor
-  odrv0.setPosition(motor_turns, velocity_feedforward, torque_feedforward);
+  odrv0.setPosition(0, 0, 0);
 
     // Read encoder angle
     uint16_t raw = AS5047P->read_raw();
@@ -343,16 +362,9 @@ void loop() {
       encoder_angle.update_angle(radians);
     }
 
-    // Read loadcell if ready
-    if (adcDataReady) {
-      adcDataReady = false;
-      weight = adc.readDataCalibrated(LOAD_CELL_SCALE) + LOAD_CELL_OFFSET;
-      loadcell_read_time_us = millis();
-      //Serial.print("time: ");
-      //Serial.print(millis() / 1000.0, 3);
-      //Serial.print(" weight: ");
-      //Serial.println(weight, 4);
-    }
+    Serial.print("loop-time (ms): ");
+    Serial.println(dt_us);
+
 
     // print data every 100(ish) ms
     if (millis() - last_print >= 100) {
@@ -374,7 +386,7 @@ void loop() {
     }
 
     // Updates for next loop
-    last_loop_us = millis();
+    last_loop_us = micros();
     sample_index++;
 
   // print position and velocity for Serial Plotter
